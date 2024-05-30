f6f90b4561b6b1dd6b3da24aba34e0b5fd38f531
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-30 13:43:55 +0200

    Input spaces and generator button created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
-import Meme
+import Meme from "./components/Meme";
 
 function App() {
   return (
     <>
       <Header />
-
+      <Meme />
     </>
   );
 }
 
 export default App;
-
