eed4840b5c2e03eeaa793180d6ed09263ffe9796
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-30 16:44:34 +0200

    File to simulate API added in the mean time, form element made into div to prevent refresh and random meme generated and the url caught in console.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,33 +1,23 @@
-import React from "react"
+import React from "react";
+import memesData from "../memesData.jsx";
 
 export default function Meme() {
-    return (
-        <main>
-            <form className="form">
-                <div>
-                    <label htmlFor="top-text">Top Text</label>
-                    <input
-                        id="top-text"
-                        type="text"
-                        placeholder="Shut up"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <label>Bottom Text
-                    <input
-                            type="text"
-                            placeholder="and take my money"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <button
-                    className="form--button"
-                >
-                    Get a new meme image 🖼
-                </button>
-            </form>
-        </main>
-    )
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+    </main>
+  );
 }
